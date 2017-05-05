dae0a6cf25d939f582f85ae2fc16e046b002e5c2
david.vilares <david.vilares@udc.es>
2017-05-05 23:45:29 +0200

    Added workaround for the TIRA evaluation
    Now tira_evaluation.py does not suffer from Dynet memory leaks caused
    when running multiple models as a part of the same execution.

---
+++ run_model.py
@@ -0,0 +1,109 @@
+
+from argparse import ArgumentParser, Namespace
+import codecs
+import sys
+import pickle
+import os
+import time
+import lysfastparse.utils
+import lysfastparse.bcovington.utils_bcovington
+import tempfile
+import yaml
+import subprocess
+import lysfastparse.bcovington.covington
+
+
+parser = ArgumentParser()
+parser.add_argument("-p", dest="p",metavar="FILE")
+parser.add_argument("-m", dest="m",metavar="FILE")
+parser.add_argument("-o", dest="o",metavar="FILE")
+parser.add_argument("-epe", dest="epe",metavar="FILE")
+parser.add_argument("-efe",dest="efe",metavar="FILE")
+parser.add_argument("-r", dest="r",help="Input run [raw|conllu]", type=str)
+parser.add_argument("-i", dest="i",metavar="FILE")
+parser.add_argument("--dynet-mem", dest="dynet_mem", help="It is needed to specify this parameter")
+    
+args = parser.parse_args()
+
+print "args (run_model.py)",args
+
+path_params = args.p
+path_model = args.m
+path_outfile = args.o
+path_pos_embeddings = args.epe
+path_feats_embeddings = args.efe
+type_text = args.r
+path_input = args.i
+
+
+valid_content = False
+    
+if type_text == "conllu" and os.path.exists(path_model):
+             
+    with codecs.open(path_input) as f:
+             
+        f_temp = tempfile.NamedTemporaryFile("w", delete=False)
+        f_temp.write(f.read())
+        f_temp.close()
+        valid_content = True
+             
+elif type_text == "raw" and os.path.exists(path_model):
+         
+    pipe = lysfastparse.utils.UDPipe(args.udpipe_model, config[YAML_UDPIPE])    
+    raw_content = lysfastparse.utils.read_raw_file(path_input)
+    conllu = pipe.run(raw_content, options=" --tokenize --tag")
+    f_temp = tempfile.NamedTemporaryFile("w", delete=False)
+    f_temp.write(conllu)
+    f_temp.close()
+    valid_content = True
+        
+
+if valid_content == True:          
+
+    #TEST PHASE
+    with codecs.open(path_params, 'r') as paramsfp:
+        aux = pickle.load(paramsfp)
+        words, w2i, lemmas, l2i, cpos , pos, feats, rels, stored_opt = aux   
+                    
+                    
+    d = vars(stored_opt)
+                
+    d["external_embedding"] = None #os.sep.join([args.e,"FB_embeddings","wiki."+metadata[LTCODE]+".vec"])    
+    d["pos_external_embedding"] = path_pos_embeddings #os.sep.join([args.e,"UD_POS_embeddings",metadata[NAME_TREEBANK]])
+    d["feats_external_embedding"] = path_feats_embeddings #os.sep.join([args.e,"UD_FEATS_embeddings",metadata[NAME_TREEBANK]])
+    d["lemmas_external_embedding"] = None
+                
+#     print d
+#     print
+    stored_opt =Namespace(**d)
+    print "Running model with this configuration", stored_opt
+#     print
+#     
+#     print "Running "+path_model
+                   
+                    
+    parser = lysfastparse.bcovington.covington.CovingtonBILSTM(words, lemmas, cpos, pos, feats, rels, w2i, l2i, stored_opt,
+                                                                                   None)
+                        
+    parser.Load(path_model)
+                        
+    with codecs.open(f_temp.name) as f_temp:
+                        
+        lookup_conll_data = lysfastparse.utils.lookup_conll_extra_data(f_temp)
+                        
+        testpath = f_temp.name 
+        ts = time.time()
+        pred = list(parser.Predict(testpath))
+        te = time.time()
+        print "Took "+str(te - ts)+" seconds"
+        lysfastparse.bcovington.utils_bcovington.write_conll(testpath, pred)
+                        
+        lysfastparse.utils.dump_lookup_extra_into_conll(testpath, lookup_conll_data)
+        lysfastparse.utils.transform_to_single_root(testpath)
+                        
+        with codecs.open(path_outfile,"w") as f_out:
+                        
+            with codecs.open(f_temp.name) as f_out_aux:
+                f_out.write(f_out_aux.read())
+            
+    os.unlink(f_temp.name)
--- tira_evaluation.py
+++ tira_evaluation.py
@@ -7,11 +7,13 @@ import codecs
 import time
 import lysfastparse.utils
 import lysfastparse.bcovington.utils_bcovington
-import lysfastparse.bcovington.covington
 import tempfile
 import yaml
+import subprocess
+import sys
 
-LTCODE="lcode"
+LCODE="lcode"
+TCODE="tcode"
 GOLDFILE="goldfile"
 OUTFILE="outfile"
 PSEGMORFILE="psegmorfile"
@@ -24,6 +26,40 @@ R_UDPIPE = "udpipe"
 
 YAML_UDPIPE=""
 
+
+def get_models_dict(path_models):
+    d = {}
+    files = [(path_models+os.sep+f,f) for f in os.listdir(path_models)]
+    for path,name in files:
+        name_split =name.split(".")
+        l,t = name_split[0],name_split[1]
+        
+        if l not in d: d[l] = {}
+        if t not in d[l]: d[l][t] = {"model":None,
+                                     "params":None}
+        
+        if name.endswith(".model"):
+            d[l][t]["model"] = path 
+        if name.endswith(".pickle"):
+            d[l][t]["params"] = path 
+    
+    return d
+    
+def select_model(lcode, tcode, dict_models):
+    
+    try:
+        #If we know the lang and treebank code
+        return dict_models[lcode][tcode]["model"],dict_models[lcode][tcode]["params"]  
+    except KeyError:
+        try:
+            #If we know the lang but not the treebank code
+            return  dict_models[lcode]["0"]["model"],dict_models[lcode]["0"]["params"]  
+        except KeyError:
+            #We do not know the lang neither the treebank code
+            return dict_models["en"]["0"]["model"],dict_models["en"]["0"]["params"]
+
+
+
 if __name__ == '__main__':
     
     parser = ArgumentParser()
@@ -32,7 +68,7 @@ if __name__ == '__main__':
     parser.add_argument("-o", dest="o",help="Output directory",metavar="FILE")
     parser.add_argument("-m", dest="m", help="Models directory",metavar="FILE")
     parser.add_argument("-e", dest="e", help="Embeddings directory",metavar="FILE")
-    parser.add_argument("--dynet-mem", dest="dynet-mem", help="It is needed to specify this parameter")
+    parser.add_argument("--dynet-mem", dest="dynet_mem", help="It is needed to specify this parameter")
     parser.add_argument("--conf", dest="conf")
     
     
@@ -40,95 +76,45 @@ if __name__ == '__main__':
     
     print "args", args
     
+    
     config = yaml.safe_load(open(args.conf))
     
     with open(args.c+os.sep+"metadata.json") as data_file:    
         metadata_datasets = json.load(data_file)
     
+    dict_models = get_models_dict(args.m)
+    
     for metadata in metadata_datasets:
         
-        path_model = os.sep.join([args.m,metadata[NAME_TREEBANK],metadata[NAME_TREEBANK]+"-"+metadata[LTCODE]+".model"])
-        path_params = os.sep.join([args.m,metadata[NAME_TREEBANK],"params.pickle"])
+        path_model, path_params = select_model(metadata[LCODE], metadata[TCODE], dict_models)
+        name_extrn_emb = path_model.rsplit("/",1)[1].split(".")[2]
         
-        
-        print path_model
-        print path_params
-        print args.r, type(args.r)
-        
-        if os.path.exists(os.sep.join([args.o,metadata[OUTFILE]])):
-            print os.sep.join([args.o,metadata[OUTFILE]]),"has been already computed"
-            continue
-        
-        
-        valid_content = False
-        
-        if args.r == "conllu" and os.path.exists(path_model):
+        print "Processing model located at",path_model
+        print "Processing params located at",path_params
+        print "Using POS and FEATs embeddings from",name_extrn_emb
                  
-            with codecs.open(os.sep.join([args.c,metadata[PSEGMORFILE]])) as f:
-                 
-                f_temp = tempfile.NamedTemporaryFile("w", delete=False)
-                f_temp.write(f.read())
-                f_temp.close()
-                valid_content = True
-                 
-        elif args.r == "raw" and os.path.exists(path_model):
-             
-            pipe = lysfastparse.utils.UDPipe(args.udpipe_model, config[YAML_UDPIPE])    
-            raw_content = lysfastparse.utils.read_raw_file(args.input)
-            conllu = pipe.run(raw_content, options=" --tokenize --tag")
-            f_temp = tempfile.NamedTemporaryFile("w", delete=False)
-            f_temp.write(conllu)
-            f_temp.close()
-            valid_content = True
-            
-    
-        if valid_content == True:            
-            #TEST PHASE
-            with codecs.open(path_params, 'r') as paramsfp:
-                aux = pickle.load(paramsfp)
-                words, w2i, lemmas, l2i, cpos , pos, feats, rels, stored_opt = aux   
-                
-            print stored_opt
-            print
-                
-            d = vars(stored_opt)
-            
-            d["external_embedding"] = None #os.sep.join([args.e,"FB_embeddings","wiki."+metadata[LTCODE]+".vec"])    
-            d["pos_external_embedding"] = os.sep.join([args.e,"UD_POS_embeddings",metadata[NAME_TREEBANK]])
-            d["feats_external_embedding"] = os.sep.join([args.e,"UD_FEATS_embeddings",metadata[NAME_TREEBANK]])
-            d["lemmas_external_embedding"] = None
-            
-            print d
-            print
-            stored_opt =Namespace(**d)
-            print stored_opt
-            print
+        path_pos_embeddings = os.sep.join([args.e,"UD_POS_embeddings",name_extrn_emb])
+        path_feats_embeddings = os.sep.join([args.e,"UD_FEATS_embeddings",name_extrn_emb])
+        path_output = os.sep.join([args.o,metadata[OUTFILE]])
+        if args.r == "conllu":
+            path_input = os.sep.join([args.c,metadata[PSEGMORFILE]])
+        elif args.r == "raw":
+            path_input = os.sep.join([args.c,metadata[RAWFILE]])
+        else:
+            raise NotImplementedError
+        
 
-            print "Running "+path_model
-                
-            parser = lysfastparse.bcovington.covington.CovingtonBILSTM(words, lemmas, cpos, pos, feats, rels, w2i, l2i, stored_opt,
-                                                                           None)
-                
-            parser.Load(path_model)
-                
-            with codecs.open(f_temp.name) as f_temp:
-                
-                lookup_conll_data = lysfastparse.utils.lookup_conll_extra_data(f_temp)
-                
-            testpath = f_temp.name 
-            ts = time.time()
-            pred = list(parser.Predict(testpath))
-            te = time.time()
-            print "Took "+str(te - ts)+" seconds"
-            lysfastparse.bcovington.utils_bcovington.write_conll(testpath, pred)
-                
-            lysfastparse.utils.dump_lookup_extra_into_conll(testpath, lookup_conll_data)
-            lysfastparse.utils.transform_to_single_root(testpath)
-                
-            with codecs.open(os.sep.join([args.o,metadata[OUTFILE]]),"w") as f_out:
-                
-                with codecs.open(f_temp.name) as f_out_aux:
-                    f_out.write(f_out_aux.read())
-    
-            os.unlink(f_temp.name)
+        if os.path.exists(path_output):
+            print path_output,"has been previously computed"
+        elif not os.path.exists(path_model):
+            print path_output,"there is no", path_model," model"
+        else:
+            command = " ".join(["python run_model.py", "-p",path_params,"-m",path_model, 
+                                "-o",path_output, "-epe", path_pos_embeddings, "-efe", path_feats_embeddings,
+                                "-r",args.r, "-i",path_input,
+                                "--dynet-mem", args.dynet_mem])
+            
+            os.system(command)
+            
+
     
